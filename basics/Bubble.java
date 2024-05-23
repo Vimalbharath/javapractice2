34dfaa05bf5c55126ea0a2cb0bf425416c3377b3
Vimalbharath <vimalbharath21@gmail.com>
2024-05-23 09:06:16 +0530

    working mixed

--- basics/Bubble.java
+++ basics/Bubble.java
@@ -27,6 +27,19 @@ public class Bubble {
 
     }
 
+    private static void insertion(int[] arr) {
+        for (int i = 0; i <= arr.length - 2; i++) {
+            for (int j = i + 1; j > 0; j--) {
+                if (arr[j - 1] > arr[j]) {
+                    swap(arr, j - 1, j);
+
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+
     private static void swap(int arr[], int i, int j) {
         int temp = arr[i];
         arr[i] = arr[i - 1];
