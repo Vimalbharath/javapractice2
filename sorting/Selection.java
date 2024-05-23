b1f97af73dbc294fc0a82a1998547c0e2b74aedf
Vimalbharath <vimalbharath21@gmail.com>
2024-05-23 20:51:34 +0530

    last change work

--- sorting/Selection.java
+++ sorting/Selection.java
@@ -12,7 +12,7 @@ public class Selection {
     private static void selectionsort(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
             int start = 0;
-            int last = arr.length - i;
+            int last = arr.length-1 - i;
             int maxindex = max(arr, start, last);
             swap(arr, maxindex, last);
         }
