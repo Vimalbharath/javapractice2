cbc185d144ef648b185ca7223d11dff601eb9d48
Vimalbharath <vimalbharath21@gmail.com>
2024-05-23 09:22:39 +0530

    index out of bound

---
+++ basics/Selection.java
@@ -0,0 +1,39 @@
+package basics;
+
+import java.util.Arrays;
+
+public class Selection {
+    public static void main(String[] args) {
+        int[] arr = { 1, 7, 6, -2, 5, 4, 3, 2 };
+        arr = selectionsort(arr);
+        // swap(arr, 1, 2);
+        int max = maxindex(arr);
+        System.out.println(max);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static int maxindex(int[] arr) {
+        int max = 0;
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] > arr[max]) {
+                max = i;
+            }
+        }
+        return max;
+    }
+
+    private static int[] selectionsort(int[] arr) {
+        for (int i = arr.length; i > 0; i--) {
+            int max = maxindex(arr);
+            swap(arr, max, i);
+        }
+
+        return arr;
+    }
+
+    private static void swap(int arr[], int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[i - 1];
+        arr[i - 1] = temp;
+    }
+}
