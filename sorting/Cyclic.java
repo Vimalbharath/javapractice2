d3a7d8d0cf5fcee030b425f76d16e1722c3ac04c
Vimalbharath <vimalbharath21@gmail.com>
2024-05-23 23:17:18 +0530

    debug

--- sorting/Cyclic.java
+++ sorting/Cyclic.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class Cyclic {
     public static void main(String[] args) {
-        int[] arr = { 8, 7, 6, 5, 4, 3, 2, 1 };
+        int[] arr = { 1, 6, 5, 4, 3, 2, 7 };
         cyclicsort3(arr);
         System.out.println(Arrays.toString(arr));
     }
