863d5f34954ccf5e7a4039a609cba16d264f2f38
Vimalbharath <vimalbharath21@gmail.com>
2024-05-25 21:06:09 +0530

    index out of bound

--- recursion/sorting/Bubble.java
+++ recursion/sorting/Bubble.java
@@ -1,5 +1,24 @@
 package recursion.sorting;
 
-public class Bubble {
+import java.util.Arrays;
 
+public class Bubble {
+    public static void main(String[] args) {
+        int[] arr = { 4, 3, 2, 1 };
+        bubblesort(arr, arr.length, 0);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static void bubblesort(int[] arr, int r, int c) {
+        if (c > r) {
+            return;
+        }
+        if (arr[c] > arr[c + 1]) {
+            int temp = arr[c];
+            arr[c] = arr[c + 1];
+            arr[c + 1] = temp;
+            bubblesort(arr, r, c + 1);
+        }
+        bubblesort(arr, r - 1, 0);
+    }
 }
