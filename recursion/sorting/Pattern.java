488f4a31c27b8b856edebb2237df8157d6d5afa5
Vimalbharath <vimalbharath21@gmail.com>
2024-05-25 20:39:33 +0530

    recursion infinity

--- recursion/sorting/Pattern.java
+++ recursion/sorting/Pattern.java
@@ -4,6 +4,21 @@ public class Pattern {
     public static void main(String[] args) {
         System.out.println("Hello");
         pattern1(5);
+        pattern2(5, 0);
+    }
+
+    private static void pattern2(int r, int c) {
+        if (r == 0) {
+            return;
+        }
+        if (c < r) {
+            System.out.print("*");
+            pattern2(r, c + 1);
+        } else {
+            System.out.println();
+            pattern2(r + 1, 0);
+        }
+
     }
 
     private static void pattern1(int n) {
