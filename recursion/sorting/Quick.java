0a71d8f6f150efb15395dbd16cd9cbdf1450d1bb
Vimalbharath <vimalbharath21@gmail.com>
2024-05-26 00:25:04 +0530

    low high no reaction

--- recursion/sorting/Quick.java
+++ recursion/sorting/Quick.java
@@ -6,26 +6,33 @@ public class Quick {
     public static void main(String[] args) {
         int[] arr = { 5, 4, 3, 2, 1 };
 
-        quicksort(arr, 0, arr.length);
+        quicksort(arr, 0, arr.length - 1);
         System.out.println(Arrays.toString(arr));
     }
 
-    private static void quicksort(int[] arr, int s, int e) {
-        if (s < e) {
+    private static void quicksort(int[] arr, int low, int high) {
+        if (low <= high) {
             return;
         }
+        int s = low;
+        int e = high;
         int mid = s + (s - e) / 2;
 
-        while (arr[e] < arr[mid]) {
-            swap(arr, e, s);
-            e--;
-        }
-        while (arr[s] > arr[mid]) {
+        while (s <= e) {
+            if (arr[e] < arr[mid]) {
+                // swap(arr, e, s);
+                e--;
+            }
+            if (arr[s] > arr[mid]) {
+                // swap(arr, s, e);
+                s++;
+            }
             swap(arr, s, e);
+            e--;
             s++;
         }
-        quicksort(arr, s, mid - 1);
-        quicksort(arr, mid + 1, e);
+        quicksort(arr, low, e);
+        quicksort(arr, s, high);
     }
 
     private static void swap(int[] arr, int s, int e) {
