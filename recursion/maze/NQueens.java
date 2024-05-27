c7e68fdbe2ddf1acfac404e6d60b5da2e95770b6
Vimalbharath <vimalbharath21@gmail.com>
2024-05-28 00:16:58 +0530

    need compile error and no ans

--- recursion/maze/NQueens.java
+++ recursion/maze/NQueens.java
@@ -17,7 +17,7 @@ public class NQueens {
         }
         int count = 0;
         for (int col = 0; col < board.length; col++) {
-            if (isSafe()) {
+            if (isSafe(board, row, col)) {
                 board[row][col] = true;
                 count = count + queens(board, row + 1);
                 board[row][col] = false;
@@ -32,8 +32,30 @@ public class NQueens {
         }
     }
 
-    private static boolean isSafe() {
+    private static boolean isSafe(boolean[][] board, int row, int col) {
+        // upside check
+        for (int i = 0; i < row; i++) {
+            if (board[i][col]) {
+                return false;
+            }
+        }
 
+        // left
+        int left = Math.min(row, col);
+        for (int i = 1; i <= left; i++) {
+            if (board[row - i][col - i]) {
+                return false;
+            }
+        }
+
+        // right
+        int right = Math.min(row, board.length - col - 1);
+        for (int i = 1; i <= right; i++) {
+            if (board[row - i][col - i]) {
+                return false;
+            }
+        }
+        return true;
     }
 
 }
