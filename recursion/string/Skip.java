da7d22e7b8a690b039f1dd803828b0b5e8204340
Vimalbharath <vimalbharath21@gmail.com>
2024-05-27 13:56:17 +0530

    else bracket

--- recursion/string/Skip.java
+++ recursion/string/Skip.java
@@ -2,7 +2,11 @@ package recursion.string;
 
 public class Skip {
     public static void main(String[] args) {
-        skipletter("", "hfkakjfhaaafa");
+        String up = "ababababa";
+        System.out.println(up.substring(1));
+        char ch = up.charAt(0);
+        System.out.println(ch);
+        skipletter("", up);
     }
 
     private static void skipletter(String p, String up) {
@@ -15,10 +19,11 @@ public class Skip {
             up = up.substring(1);
 
             skipletter(p, up);
+        } else {
+            up = up.substring(1);
+            p = p + ch;
+            skipletter(p, up);
         }
-        up = up.substring(1);
-        p = p + ch;
-        skipletter(p, up);
 
     }
 }
