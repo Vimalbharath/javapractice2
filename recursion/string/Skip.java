73f504fbda9bf67c1c4f444cc58e352fc8acfa03
Vimalbharath <vimalbharath21@gmail.com>
2024-05-27 13:36:24 +0530

    infinite

--- recursion/string/Skip.java
+++ recursion/string/Skip.java
@@ -2,18 +2,23 @@ package recursion.string;
 
 public class Skip {
     public static void main(String[] args) {
-        skipletter("hfkakjfhaaafa");
+        skipletter("", "hfkakjfhaaafa");
     }
 
-    private static void skipletter(String up) {
+    private static void skipletter(String p, String up) {
         if (up.isEmpty()) {
-            System.out.println(up);
+            System.out.println(p);
             return;
         }
         char ch = up.charAt(0);
         if (ch == 'a') {
             up = up.substring(1);
+
+            skipletter(p, up);
         }
-        up = ch + up.substring(1);
+        up = up.substring(1);
+        p = p + ch;
+        skipletter(p, up);
+
     }
 }
