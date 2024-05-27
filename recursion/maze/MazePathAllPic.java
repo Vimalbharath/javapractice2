fd1f94c5716e7034fee2d92e768d5d8d30104814
Vimalbharath <vimalbharath21@gmail.com>
2024-05-27 22:57:42 +0530

    Great!

--- recursion/maze/MazePathAllPic.java
+++ recursion/maze/MazePathAllPic.java
@@ -10,12 +10,13 @@ public class MazePathAllPic {
         };
 
         int[][] path = new int[box.length][box[0].length];
-        path(0, 0, "", box, path);
+        path(0, 0, "", box, path, 1);
         // System.out.println(ans);
     }
 
-    private static void path(int r, int c, String ch, boolean[][] box, int[][] path) {
+    private static void path(int r, int c, String ch, boolean[][] box, int[][] path, int sum) {
         if (r == box.length - 1 && c == box[0].length - 1) {
+            path[c][r] = sum;
             System.out.println(ch);
 
             for (int i = 0; i < path.length; i++) {
@@ -31,21 +32,22 @@ public class MazePathAllPic {
         // String ch = "";
 
         if (box[c][r]) {
-            path[c][r] = 1;
+            path[c][r] = sum;
             box[c][r] = false;
             if (r < box.length - 1) {
-                path(r + 1, c, ch + "R", box, path);
+                path(r + 1, c, ch + "R", box, path, sum + 1);
             }
             if (c < box[0].length - 1) {
-                path(r, c + 1, ch + "D", box, path);
+                path(r, c + 1, ch + "D", box, path, sum + 1);
             }
             if (r > 0) {
-                path(r - 1, c, ch + "L", box, path);
+                path(r - 1, c, ch + "L", box, path, sum + 1);
             }
             if (c > 0) {
-                path(r, c - 1, ch + "U", box, path);
+                path(r, c - 1, ch + "U", box, path, sum + 1);
             }
             box[c][r] = true;
+            path[c][r] = 0;
         }
 
     }
