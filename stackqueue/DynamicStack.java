8aa478d8a41c9a949ed46fd6e779355f37b44c0e
Vimalbharath <vimalbharath21@gmail.com>
2024-06-02 23:05:21 +0530

    dynamic not working

---
+++ stackqueue/DynamicStack.java
@@ -0,0 +1,33 @@
+package stackqueue;
+
+public class DynamicStack extends CustomStack {
+
+    @Override
+    public boolean push(int item) {
+        if (isFull()) {
+            int[] temp = new int[ptr * 2];
+            for (int i = 0; i < data.length; i++) {
+                temp[i] = data[i];
+            }
+        }
+        return super.push(item);
+    }
+
+    public static void main(String[] args) throws Exception {
+        DynamicStack stack = new DynamicStack();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        stack.push(5);
+        stack.push(6);
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        stack.push(5);
+        stack.push(6);
+
+        System.out.println(stack.pop());
+    }
+}
