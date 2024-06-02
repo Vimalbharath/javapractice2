6799020ea56a5b5dc1c71358f15dbad3b7c568cf
Vimalbharath <vimalbharath21@gmail.com>
2024-06-02 12:32:40 +0530

    display infinite

---
+++ linkedlist/DoubleLL.java
@@ -0,0 +1,60 @@
+package linkedlist;
+
+public class DoubleLL {
+    private Node head;
+    private int size;
+
+    private class Node {
+        int val;
+        Node next;
+        Node prev;
+
+        public Node(int val, Node next, Node prev) {
+            this.val = val;
+            this.next = next;
+            this.prev = prev;
+        }
+
+        public Node(int val, Node next) {
+            this.val = val;
+            this.next = next;
+        }
+
+        public Node(int val) {
+            this.val = val;
+        }
+
+    }
+
+    public DoubleLL() {
+        this.size = 0;
+    }
+
+    public void addFirst(int value) {
+        Node node = new Node(value);
+        if (head == null) {
+            head = node;
+        }
+        node.next = head;
+        node = head;
+        size = size + 1;
+    }
+
+    public void display() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.val + " -> ");
+            temp = temp.next;
+        }
+        System.out.print("END");
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        DoubleLL list = new DoubleLL();
+        list.addFirst(1);
+        list.addFirst(2);
+        list.display();
+    }
+
+}
