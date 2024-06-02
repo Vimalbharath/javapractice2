67be5325b800a512171fcb49f48d40a4cf631094
Vimalbharath <vimalbharath21@gmail.com>
2024-06-02 22:06:29 +0530

    custom stack looks good with push pop

--- stackqueue/CustomStack.java
+++ stackqueue/CustomStack.java
@@ -1,14 +1,52 @@
 package stackqueue;
 
 public class CustomStack {
+    protected int[] data;
     public int ptr = -1;
     private static final int DEFAULT_SIZE = 10;
 
     CustomStack() {
-        super(DEFAULT_SIZE);
+        this(DEFAULT_SIZE);
     }
 
     CustomStack(int size) {
-        int[] data = new int[size];
+        this.data = new int[size];
+    }
+
+    public boolean push(int item) {
+        if (isFull()) {
+            System.out.println("Stack is full");
+            return false;
+        }
+        ptr++;
+        data[ptr] = item;
+        return true;
+    }
+
+    public int pop() throws Exception {
+        if (isEmpty()) {
+            throw new Exception("Stack is Empty");
+        }
+        return data[ptr--];
+    }
+
+    private boolean isFull() {
+        return ptr == data.length - 1;
+    }
+
+    private boolean isEmpty() {
+        return ptr == -1;
+    }
+
+    public static void main(String[] args) throws Exception {
+        CustomStack stack = new CustomStack(5);
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        stack.push(5);
+
+        System.out.println(stack.pop());
+
     }
 }
