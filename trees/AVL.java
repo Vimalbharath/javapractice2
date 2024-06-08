5af207fe0c19c875742b2a6b45d58702e0e34010
Vimalbharath <vimalbharath21@gmail.com>
2024-06-08 20:02:48 +0530

    good

--- trees/AVL.java
+++ trees/AVL.java
@@ -40,11 +40,49 @@ public class AVL extends BST {
                 return rightRotate(node);
             }
         }
+        if (height(node.left) - height(node.right) < -1) {
+            // left heavy
+            if (height(node.right.left) - height(node.right.right) < 0) {
+                // left left
+                return leftRotate(node);
+            }
+            if (height(node.right.left) - height(node.right.right) > 0) {
+                // left right
+                node.right = rightRotate(node.left);
+                return leftRotate(node);
+            }
+        }
+        return node;
+    }
+
+    private Node rightRotate(Node p) {
+        Node c = p.left;
+        Node t = c.right;
+
+        c.right = p;
+        p.left = t;
+
+        p.height = Math.max(height(p.left), height(p.right) + 1);
+        c.height = Math.max(height(c.left), height(c.right) + 1);
+        return c;
+    }
+
+    private Node leftRotate(Node c) {
+
+        Node p = c.right;
+        Node t = p.left;
+
+        p.left = c;
+        c.right = t;
+
+        p.height = Math.max(height(p.left), height(p.right) + 1);
+        c.height = Math.max(height(c.left), height(c.right) + 1);
+        return p;
     }
 
     public static void main(String[] args) {
-        BST tree = new BST();
-        int[] nums = { 5, 2, 7, 1, 4, 6, 9, 8, 3, 10 };
+        AVL tree = new AVL();
+        int[] nums = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
         tree.populate(nums);
         tree.preOrder(root);
         tree.display();
