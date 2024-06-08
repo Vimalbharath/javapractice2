e57b77a4e8d741768cebfeeca622e49ee7af7321
Vimalbharath <vimalbharath21@gmail.com>
2024-06-08 16:08:21 +0530

    pickup

--- trees/AVL.java
+++ trees/AVL.java
@@ -8,6 +8,40 @@ public class AVL extends BST {
         return Math.abs(height(node.left) - height(node.right)) <= 1 && balanced(node.left) && balanced(node.right);
     }
 
+    public void insert(int value) {
+        root = insert(value, root);
+    }
+
+    private Node insert(int value, Node node) {
+        if (node == null) {
+            node = new Node(value);
+            return node;
+        }
+        if (value < node.value) {
+            node.left = insert(value, node.left);
+        }
+        if (value > node.value) {
+            node.right = insert(value, node.right);
+        }
+        node.height = Math.max(height(node.left), height(node.right)) + 1;
+        return rotate(node);
+    }
+
+    private Node rotate(Node node) {
+        if (height(node.left) - height(node.right) > 1) {
+            // left heavy
+            if (height(node.left.left) - height(node.left.right) > 0) {
+                // left left
+                return rightRotate(node);
+            }
+            if (height(node.left.left) - height(node.left.right) < 0) {
+                // left right
+                node.left = leftRotate(node.left);
+                return rightRotate(node);
+            }
+        }
+    }
+
     public static void main(String[] args) {
         BST tree = new BST();
         int[] nums = { 5, 2, 7, 1, 4, 6, 9, 8, 3, 10 };
