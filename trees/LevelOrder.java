84087bd8687d5b7da1a0cf4faf5387ec065bc61a
Vimalbharath <vimalbharath21@gmail.com>
2024-06-11 22:31:05 +0530

    used c++ code

--- trees/LevelOrder.java
+++ trees/LevelOrder.java
@@ -1,44 +1,72 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- * int val;
- * TreeNode *left;
- * TreeNode *right;
- * TreeNode() : val(0), left(nullptr), right(nullptr) {}
- * TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- * TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
- * };
- */
 
 //https://leetcode.com/problems/binary-tree-level-order-traversal/description/
 package trees;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 public class LevelOrder {
 
-public: vector<vector<int>> level(TreeNode root){
-    ArrayList<ArrayList<Integer>> result=new ArrayList<>();
-    if(root==null){
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode() {}
+     * TreeNode(int val) { this.val = val; }
+     * TreeNode(int val, TreeNode left, TreeNode right) {
+     * this.val = val;
+     * this.left = left;
+     * this.right = right;
+     * }
+     * }
+     */
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            ArrayList<Integer> currentLevel = new ArrayList<>();
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode currentNode = queue.poll();
+                currentLevel.add(currentNode.val);
+                if (currentNode.left != null) {
+                    queue.offer(currentNode.left);
+                }
+                if (currentNode.right != null) {
+                    queue.offer(currentNode.right);
+                }
+            }
+            result.add(currentLevel);
+        }
         return result;
     }
 
-    Queue <TreeNode> queue=new LinkedList<>();
-    queue.offer(root);
-    while(!queue.isEmpty()){
-        int levelSize=queue.size();
-ArrayList<Integer> currentLevel=new ArrayList<>();
-        for(int i=0;i<levelSize;i++){
-            TreeNode currentNode=queue.poll();
-            currentLevel.add(currentNode.val);
-            if(currentNode.left!=null){
-                queue.offer((currentNode.left);
-            }
-            if(currentNode.right!=null){
-                queue.offer((currentNode.right);
-            }
-        }
-        result.add(currentLevel);
-    }
-return result;
 }
-};
