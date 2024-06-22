31e062e6f605f06362dacffdb940ce033a8b9a26
Vimalbharath <vimalbharath21@gmail.com>
2024-06-22 15:21:03 +0530

    with memo

--- dp/stairs.java
+++ dp/stairs.java
@@ -1,5 +1,8 @@
 package dp;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class stairs {
     public int climbStairs(int n) {
         int[] array = new int[45];
@@ -10,4 +13,25 @@ public class stairs {
         }
         return array[n - 1];
     }
+
+    private static Map<Integer, Integer> memo = new HashMap<>();
+
+    public int climbStairs2(int n) {
+        if (n == 0)
+            return 1;
+        if (n < 2) {
+            return n;
+        }
+        // Return cached result if it exists
+        if (memo.containsKey(n)) {
+            return memo.get(n);
+        }
+
+        // Compute and cache the result if it's not already in the cache
+
+        int result = climbStairs2(n - 2) + climbStairs2(n - 1);
+        memo.put(n, result);
+        return result;
+    }
+
 }
