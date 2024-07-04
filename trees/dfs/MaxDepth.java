966feb2f214391c3bc0473b4852d47ea44a12e5c
Vimalbharath <vimalbharath21@gmail.com>
2024-07-04 22:41:34 +0530

    single try

---
+++ trees/dfs/MaxDepth.java
@@ -0,0 +1,34 @@
+package trees.dfs;
+
+public class MaxDepth {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    int depth = 0;
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = maxDepth(root.left) + 1;
+        int right = maxDepth(root.right) + 1;
+        depth = Math.max(left, right);
+        return depth;
+    }
+}
