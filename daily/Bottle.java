4e344bf48395bc3010f29619858689798249f56e
Vimalbharath <vimalbharath21@gmail.com>
2024-07-07 21:29:36 +0530

    solved without seeing solution

--- daily/Bottle.java
+++ daily/Bottle.java
@@ -2,7 +2,7 @@ package daily;
 
 public class Bottle {
     public int numWaterBottles(int numBottles, int numExchange) {
-        int ans = 0;
+        int ans = numBottles;
         while (numBottles >= numExchange) {
             int ab = numBottles / numExchange;
             ans = ans + ab;
