a43dccc7b7366af2323c05065f58945f9ff8c578
Vimalbharath <vimalbharath21@gmail.com>
2024-07-08 20:18:35 +0530

    circular queue 1 to n

---
+++ daily/CircularGame.java
@@ -0,0 +1,13 @@
+package daily;
+
+public class CircularGame {
+ public int findTheWinner(int n, int k) {
+        boolean arr[n]=true;
+        if(k<n){
+            arr[k]=false;
+        }
+        for(int i=0;i<arr.length;i++){
+            
+        }
+    }
+}
