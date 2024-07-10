eaf39f64d4989c48c26bdaa6b216e993b10f6bad
Vimalbharath <vimalbharath21@gmail.com>
2024-07-10 12:26:42 +0530

    copy paste

---
+++ daily/LogFolder.java
@@ -0,0 +1,21 @@
+package daily;
+
+import java.util.Stack;
+
+public class LogFolder {
+    public int minOperations(String[] logs) {
+        Stack<String> paths_stack = new Stack<>();
+
+        for (String log : logs) {
+            if (log.equals("../")) {
+                if (!paths_stack.isEmpty()) {
+                    paths_stack.pop();
+                }
+            } else if (!log.equals("./")) {
+                paths_stack.push(log);
+            }
+        }
+
+        return paths_stack.size();
+    }
+}
