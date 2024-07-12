c4676d20eb27e7a970914020a05b693fe52a3c47
Vimalbharath <vimalbharath21@gmail.com>
2024-07-12 07:58:34 +0530

    try

--- daily/Stringab.java
+++ daily/Stringab.java
@@ -3,10 +3,31 @@ package daily;
 public class Stringab {
 
     public int maximumGain(String s, int x, int y) {
-        if (s.length() == 0) {
+        // if (s.length() == 0) {
+        // return Math.max(x, y);
+        // }
+        // char ch = s.charAt(0);
+        String up = s;
+        // String p = "";
+        return skipletter(up, 0, 0);
+
+    }
+
+    public int skipletter(String up, int x, int y) {
+        if (up.isEmpty()) {
+
             return Math.max(x, y);
         }
-        char ch = s.charAt(0);
+        // char ch = up.charAt(0);
+        if (up.startsWith("ab")) {
 
+            return skipletter(up.substring(2), x + 1, y);
+        }
+        if (up.startsWith("ba")) {
+
+            return skipletter(up.substring(2), x, y + 1);
+        }
+
+        return Math.max(x, y);
     }
 }
