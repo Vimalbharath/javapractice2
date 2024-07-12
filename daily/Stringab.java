3f6785f343e830fe000afac97e19ee8b774b18db
Vimalbharath <vimalbharath21@gmail.com>
2024-07-12 07:46:29 +0530

    rec p and up

---
+++ daily/Stringab.java
@@ -0,0 +1,12 @@
+package daily;
+
+public class Stringab {
+
+    public int maximumGain(String s, int x, int y) {
+        if (s.length() == 0) {
+            return Math.max(x, y);
+        }
+        char ch = s.charAt(0);
+
+    }
+}
