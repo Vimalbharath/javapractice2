8144c56df43f3b6d1891ad2fb9f071844d844be5
Vimalbharath <vimalbharath21@gmail.com>
2024-07-16 00:07:53 +0530

    bfs ans topo sort

--- Graphs/LoudRich.java
+++ Graphs/LoudRich.java
@@ -3,6 +3,7 @@ package Graphs;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.*;
 
 public class LoudRich {
 
@@ -39,5 +40,41 @@ public class LoudRich {
             }
         }
         return dp[ind] = minIndex;
+    }
+
+    public int[] loudAndRich1(int[][] richer, int[] quiet) {
+        int n = quiet.length;
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            adj.add(new ArrayList<>());
+        }
+        for (int[] arr : richer) {
+            adj.get(arr[1]).add(arr[0]);
+        }
+
+        int[] ans = new int[n];
+        Arrays.fill(ans, -1); // Initialize all answers to -1
+        for (int i = 0; i < n; i++) {
+            if (ans[i] == -1) { // If answer for this person is not yet calculated
+                dfs(adj, quiet, ans, i);
+            }
+        }
+        return ans;
+    }
+
+    private int dfs(ArrayList<ArrayList<Integer>> adj, int[] quiet, int[] ans, int src) {
+        if (ans[src] != -1) {
+            return ans[src]; // Return if answer for this person is already calculated
+        }
+        int minQuiet = src; // Initialize with current person
+        for (int next : adj.get(src)) {
+            int candidate = dfs(adj, quiet, ans, next); // Recursively find the least quiet person among richer
+                                                        // acquaintances
+            if (quiet[minQuiet] > quiet[candidate]) {
+                minQuiet = candidate; // Update minQuiet if a quieter person is found
+            }
+        }
+        ans[src] = minQuiet; // Store the result for current person
+        return minQuiet;
     }
 }
