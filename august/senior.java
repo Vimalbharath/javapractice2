06312c7607ff9d8dd18eacb65a43ad2a4db73763
Vimalbharath <vimalbharath21@gmail.com>
2024-08-02 23:00:34 +0530

    char to int

--- august/senior.java
+++ august/senior.java
@@ -1,13 +1,26 @@
 package august;
 
 public class senior {
-    public int countSeniors(String[] details) {
+    public static void main(String[] args) {
+        String[] details = { "1313579440F2036", "2921522980M5644" };
+        countSeniors(details);
+
+    }
+
+    public static int countSeniors(String[] details) {
         int count = 0;
         for (int i = 0; i < details.length; i++) {
             String vimal = details[i];
-            int ten = vimal.charAt(11);
-            int eleven = vimal.charAt(12);
-            int age = (ten * 10) + eleven;
+            System.out.println(vimal);
+            char ten = vimal.charAt(11);
+            System.out.println(ten);
+            char eleven = vimal.charAt(12);
+            int a = ten - '0';
+            int b = eleven - '0';
+            // int age = (ten * 10) + eleven ;
+            int age = (a * 10) + b;
+            System.out.println(eleven);
+            System.out.println(age);
 
             if (age > 60) {
                 count = count + 1;
