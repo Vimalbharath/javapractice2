46a0d15581ee5015ea41f7d3a5c12cb2d0cf5354
Vimalbharath <vimalbharath21@gmail.com>
2024-08-04 21:21:51 +0530

    out of bound

---
+++ gfg/google/arrays/wavearray.java
@@ -0,0 +1,18 @@
+package gfg.google.arrays;
+
+public class wavearray {
+
+    public static void convertToWave(int n, int[] arr) {
+        // code here
+        for (int i = 0; i < arr.length; i = i + 2) {
+            swap(i, i + 1, arr);
+        }
+    }
+
+    public static void swap(int a, int b, int[] arr) {
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+
+    }
+}
