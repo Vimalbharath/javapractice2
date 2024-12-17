d7d9574c7beab4ed01ded060379637f3658d96f2
Vimalbharath <vimalbharath21@gmail.com>
2024-12-17 13:54:18 +0530

    program not ending

---
+++ practice3/basics/cyclic.java
@@ -0,0 +1,33 @@
+package practice3.basics;
+
+import java.util.Arrays;
+
+public class cyclic {
+    public static void main(String[] args) {
+        int arr[] = { 5, 8, 9, 3, 4, 6, 7, 2, 1 };
+        System.out.println(Arrays.toString(arr));
+        cyclicsort(arr);
+        System.out.println(Arrays.toString(arr));
+
+    }
+
+    public static void cyclicsort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int correct = arr[i] + 1;
+            int j = 0;
+            while (j < arr.length) {
+                if (arr[j] != correct) {
+                    swap(arr, j, correct);
+                }
+            }
+        }
+
+    }
+
+    public static void swap(int arr[], int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+
+    }
+}
