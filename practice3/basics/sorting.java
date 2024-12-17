d5e11890e72b39d2a296141c3240169b48c48a82
Vimalbharath <vimalbharath21@gmail.com>
2024-12-17 13:05:31 +0530

    increasing best case

--- practice3/basics/sorting.java
+++ practice3/basics/sorting.java
@@ -12,11 +12,17 @@ public class sorting {
     }
 
     public static void bubblesort(int[] arr) {
+        boolean swapped;
         for (int i = 0; i < arr.length; i++) {
+            swapped = false;
             for (int j = 1; j < arr.length; j++) {
                 if (arr[j - 1] > arr[j]) {
                     swap(arr, j, j - 1);
+                    swapped = true;
                 }
+            }
+            if (!swapped) {
+                break;
             }
         }
     }
