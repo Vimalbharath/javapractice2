4a6f923b1adf918c66a0f52d57d8c7bc7ef269fb
Vimalbharath <vimalbharath21@gmail.com>
2024-12-17 13:41:10 +0530

    success inseroin

--- practice3/basics/sorting.java
+++ practice3/basics/sorting.java
@@ -12,8 +12,8 @@ public class sorting {
     }
 
     public static void insertion(int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 1; j > i; j--) {
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = i; j > 0; j--) {
                 if (arr[j] < arr[j - 1]) {
                     swap(arr, j, j - 1);
                 }
