026e6e5a35007cd221d92678ebd6b41b3f66e9b2
Vimalbharath <vimalbharath21@gmail.com>
2024-12-17 12:55:54 +0530

    bubble sort not working

---
+++ practice3/basics/sorting.java
@@ -0,0 +1,31 @@
+package practice3.basics;
+
+import java.util.Arrays;
+
+public class sorting {
+    public static void main(String[] args) {
+        int arr[] = { 5, 8, 9, 3, 4, 6, 7, 2, 1 };
+        System.out.println(Arrays.toString(arr));
+        bubblesort(arr);
+        System.out.println(Arrays.toString(arr));
+
+    }
+
+    public static void bubblesort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 1; j < arr.length; j++) {
+                if (arr[j - 1] > arr[j]) {
+                    swap(arr, j, j - 1);
+                }
+            }
+        }
+    }
+
+    public static void swap(int arr[], int first, int second) {
+        int temp = first;
+        arr[first] = arr[second];
+        arr[second] = temp;
+
+    }
+
+}
