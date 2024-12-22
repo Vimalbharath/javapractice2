2c76f87fa4004c353e95fe3b3bcd98631be71c3f
Vimalbharath <vimalbharath21@gmail.com>
2024-12-22 11:35:00 +0530

    add first display

---
+++ practice3/linked/Single.java
@@ -0,0 +1,62 @@
+package practice3.linked;
+
+public class Single {
+
+    public class Node {
+        int value;
+        Node next;
+
+        public Node() {
+
+        }
+
+        public Node(int value) {
+            this.value = value;
+        }
+
+        public Node(int value, Node next) {
+            this.value = value;
+            this.next = next;
+        }
+
+    }
+
+    public Node head;
+    public Node tail;
+    public int size;
+
+    public Single() {
+        this.size = 0;
+    }
+
+    public void addFirst(int value) {
+        Node node = new Node(value);
+        if (head == null) {
+            head = node;
+            return;
+        }
+        node.next = head;
+        head = node;
+        size = size + 1;
+
+    }
+
+    public void display() {
+        Node node = head;
+        do {
+            if (node.next != null)
+                System.out.print(node.value);
+            System.out.print("->");
+        } while (node.next != null);
+        System.out.print("NULL");
+
+    }
+
+    public static void main(String[] args) {
+        Single ll = new Single();
+        ll.addFirst(1);
+        ll.addFirst(2);
+        ll.addFirst(3);
+        ll.display();
+    }
+}
