fac20828befdad15fa47a0044b3e0a115d6b1eba
Vimalbharath <vimalbharath21@gmail.com>
2024-12-28 19:48:06 +0530

    how to put two key for one

--- practice3/dp/grid.java
+++ practice3/dp/grid.java
@@ -1,15 +1,22 @@
 package practice3.dp;
 
+import java.util.HashMap;
+
 public class grid {
     public static void main(String[] args) {
-        int ans = gridways(3, 3);
+        Long ans = gridways(18, 18);
         System.out.println(ans);
     }
 
-    public static int gridways(int row, int col) {
+    static HashMap<Integer, Long> memo = new HashMap<>();
+    static Long result = 0l;
+
+    public static Long gridways(int row, int col) {
+        if (memo.containsKey(row,col))
         if (row == 1 || col == 1) {
-            return 1;
+            return 1l;
         }
-        return gridways(row - 1, col) + gridways(row, col - 1);
+        result= gridways(row - 1, col) + gridways(row, col - 1);
+        memo.put
     }
 }
