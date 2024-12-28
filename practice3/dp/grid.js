8147a3ea28dfab8911062e0bb68588611df40f39
Vimalbharath <vimalbharath21@gmail.com>
2024-12-28 19:58:57 +0530

    with memo success use string

--- practice3/dp/grid.js
+++ practice3/dp/grid.js
@@ -1,6 +1,9 @@
-const grid=(row,col)=>{
+const grid=(row,col,memo={})=>{
+    const key=row+','+col;
+    if(key in memo) return memo[key]
     if(row==1||col===1) return 1;
-    return grid(row-1,col)+grid(row,col-1);
+    memo[key]= grid(row-1,col,memo)+grid(row,col-1,memo);
+    return memo[key];
 }
 
 console.log(grid(18,18))  
