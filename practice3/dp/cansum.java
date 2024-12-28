47ae12d1f13edee1ed560bc5fd0ba3323ce3205f
Vimalbharath <vimalbharath21@gmail.com>
2024-12-28 20:57:39 +0530

    nice work

--- practice3/dp/cansum.java
+++ practice3/dp/cansum.java
@@ -1,14 +1,21 @@
 package practice3.dp;
 
+import java.util.HashMap;
+
 public class cansum {
     public static void main(String[] args) {
-        int[] arr = { 5, 3, 4, 7 };
-        int sum = 7;
+        int[] arr = { 7, 14 };
+        int sum = 300;
         boolean ans = cansum1(arr, sum);
         System.out.println(ans);
     }
 
+    public static HashMap<Integer, Boolean> memo = new HashMap<>();
+
     public static boolean cansum1(int[] arr, int sum) {
+        if (memo.containsKey(sum)) {
+            return memo.get(sum);
+        }
         if (sum < 0) {
             return false;
         }
@@ -19,11 +26,13 @@ public class cansum {
             // if (number <= sum) {
             // sum=sum-number;
             if (cansum1(arr, sum - number) == true) {
+                memo.put(sum - number, true);
                 return true;
             }
             // }
             // cansum1(arr, sum);
         }
+        memo.put(sum, false);
         return false;
 
     }
