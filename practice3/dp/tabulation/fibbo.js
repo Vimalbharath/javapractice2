c38e3960b5c2dae24864d4661cea693c9c5fd1c7
Vimalbharath <vimalbharath21@gmail.com>
2024-12-29 08:42:10 +0530

    out of bound fix

--- practice3/dp/tabulation/fibbo.js
+++ practice3/dp/tabulation/fibbo.js
@@ -1,9 +1,9 @@
 const fibbo=(n)=>{
     const table=Array(n+1).fill(0);
     table[1]=1;
-    for(let i=0;i<table.length;i++){
-        table[i+1]+=table[i];
-        table[i+2]+=table[i];
+    for(let i=0;i<=table.length;i++){
+        if(table[i+1]<table.length) table[i+1]+=table[i];
+        if(table[i+2]<table.length) table[i+2]+=table[i];
     }
     return table[n];
 }
