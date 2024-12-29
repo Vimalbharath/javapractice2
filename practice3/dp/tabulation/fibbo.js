e58e5bb10f3ac49f33370f80d7bd34a675e69718
Vimalbharath <vimalbharath21@gmail.com>
2024-12-29 08:37:28 +0530

    too much error

---
+++ practice3/dp/tabulation/fibbo.js
@@ -0,0 +1,11 @@
+const fibbo=(n)=>{
+    const table=Array(n+1).fill(0);
+    table[1]=1;
+    for(let i=0;i<table.length;i++){
+        table[i+1]+=table[i];
+        table[i+2]+=table[i];
+    }
+    return table[n];
+}
+
+console.log(fibbo(8));
